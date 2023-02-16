44ff1584d2ea995829896c791a25930ad27003c0
Adil Chetouani <chetouani.adil93@gmail.com>
2023-02-16 16:19:16 +0100

    Init class QuestionDTO, QuestionnaireDTO and FichierQuestionnaireBO

--- src/fr/iut/montreuil/S4_R02_2023_06_QuizzUp_Questionnaire_sme/entities/dto/QuestionDTO.java
+++ src/fr/iut/montreuil/S4_R02_2023_06_QuizzUp_Questionnaire_sme/entities/dto/QuestionDTO.java
@@ -1,6 +1,80 @@
 package fr.iut.montreuil.S4_R02_2023_06_QuizzUp_Questionnaire_sme.entities.dto;
 
 public class QuestionDTO {
-    public QuestionDTO() {
+    private int numero;
+    private String langue;
+    private String libelle;
+    private String reponse;
+    private int difficulte;
+    private String explication;
+    private String reference;
+
+
+    public QuestionDTO(int numero, String langue, String libelle, String reponse, int difficulte, String explication, String reference) {
+        this.numero = numero;
+        this.langue = langue;
+        this.libelle = libelle;
+        this.reponse = reponse;
+        this.difficulte = difficulte;
+        this.explication = explication;
+        this.reference = reference;
     }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getLangue() {
+        return langue;
+    }
+
+    public void setLangue(String langue) {
+        this.langue = langue;
+    }
+
+    public String getLibelle() {
+        return libelle;
+    }
+
+    public void setLibelle(String libelle) {
+        this.libelle = libelle;
+    }
+
+    public String getReponse() {
+        return reponse;
+    }
+
+    public void setReponse(String reponse) {
+        this.reponse = reponse;
+    }
+
+    public int getDifficulte() {
+        return difficulte;
+    }
+
+    public void setDifficulte(int difficulte) {
+        this.difficulte = difficulte;
+    }
+
+    public String getExplication() {
+        return explication;
+    }
+
+    public void setExplication(String explication) {
+        this.explication = explication;
+    }
+
+    public String getReference() {
+        return reference;
+    }
+
+    public void setReference(String reference) {
+        this.reference = reference;
+    }
+
+    
 }
--- src/fr/iut/montreuil/S4_R02_2023_06_QuizzUp_Questionnaire_sme/entities/dto/QuestionnaireDTO.java
+++ src/fr/iut/montreuil/S4_R02_2023_06_QuizzUp_Questionnaire_sme/entities/dto/QuestionnaireDTO.java
@@ -1,6 +1,31 @@
 package fr.iut.montreuil.S4_R02_2023_06_QuizzUp_Questionnaire_sme.entities.dto;
 
+import java.util.ArrayList;
+
 public class QuestionnaireDTO {
-    public QuestionnaireDTO() {
+    private int id_questionnaire;
+    private ArrayList<QuestionDTO> questions;
+
+    public QuestionnaireDTO(int id_questionnaire, ArrayList<QuestionDTO> questions) {
+        this.id_questionnaire = id_questionnaire;
+        this.questions = questions;
     }
+
+    public int getId_questionnaire() {
+        return id_questionnaire;
+    }
+
+    public void setId_questionnaire(int id_questionnaire) {
+        this.id_questionnaire = id_questionnaire;
+    }
+
+    public ArrayList<QuestionDTO> getQuestions() {
+        return questions;
+    }
+
+    public void setQuestions(ArrayList<QuestionDTO> questions) {
+        this.questions = questions;
+    }
+
+    
 }
