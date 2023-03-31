8a8884698439ec88ad233bfdacfad33385a77f0b
ismailSKR <ismailgada94@gmail.com>
2023-03-31 11:51:03 +0200

    Changement Stats Quest

--- src/fr/iut/montreuil/S4_R02_2023_06_QuizzUp_Questionnaire_sme/entities/dto/StatsQuestDTO.java
+++ src/fr/iut/montreuil/S4_R02_2023_06_QuizzUp_Questionnaire_sme/entities/dto/StatsQuestDTO.java
@@ -4,34 +4,17 @@ package fr.iut.montreuil.S4_R02_2023_06_QuizzUp_Questionnaire_sme.entities.dto;
 public class StatsQuestDTO{
     private int nbjouer;
     private int nbOk;
-    private int difficulte;
     private int numero;
 
-    public StatsQuestDTO(int numero) {
-        this.nbjouer = 0;
-        this.nbOk = 0;
-        this.numero=numero;
+    public StatsQuestDTO(int nbjouer, int nbOk, int numero) {
+        this.nbjouer = nbjouer;
+        this.nbOk = nbOk;
+        this.numero = numero;
     }
 
     public StatsQuestDTO() {
         this.nbjouer = 0;
         this.nbOk = 0;
-    }
-
-    public int getDifficulte() {
-        return difficulte;
-    }
-
-    public void setDifficulte(int difficulte) {
-        this.difficulte = difficulte;
-    }
-
-    public int getNumero() {
-        return numero;
-    }
-
-    public void setNumero(int numero) {
-        this.numero = numero;
     }
 
     public int getNbjouer() {
@@ -58,12 +41,17 @@ public class StatsQuestDTO{
         nbOk++;
     }
 
+    public int getNumero() {
+        return numero;
+    }
 
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
 
     @Override
     public String toString() {
-        return "StatsQuestDTO [nbjouer=" + nbjouer + ", nbOk=" + nbOk + "]";
+        return "StatsQuestDTO [nbjouer=" + nbjouer + ", nbOk=" + nbOk + ", numero=" + numero + "]";
     }
-
 
 }
--- src/fr/iut/montreuil/S4_R02_2023_06_QuizzUp_Questionnaire_sme/impl/ServiceStatsImpl.java
+++ src/fr/iut/montreuil/S4_R02_2023_06_QuizzUp_Questionnaire_sme/impl/ServiceStatsImpl.java
@@ -60,7 +60,7 @@ public class ServiceStatsImpl implements IServiceStatsQuestionnaire {
                 throw new StatsQuestionsIncorrectExeptions();
             }
 
-            StatsQuestDTO statsQuestion = new StatsQuestDTO(idQuestion);
+            StatsQuestDTO statsQuestion = new StatsQuestDTO(nbDeFoisJoueQuestion, nbDeFoisReponseCorrecte, idQuestion);
             statsQuestions.add(statsQuestion);
         }
 
